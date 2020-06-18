3ea807066c2de4e08bdc56d9a16a1e8285268078
retegi84 <retegi84@gmail.com>
2020-06-18 20:47:46 +0200

    Mejora del programa con validación de dato insertado, función, return y try

--- tarea21/tarea21.py
+++ tarea21/tarea21.py
@@ -1,30 +1,51 @@
-#Mínimo y máximo
+        #THE_EGG TAREA21 IÑAKI RETEGI 18/06/2020
+
+#ALGORITMO
+def fraccion(decimal):
+    #INICIALIZACIÓN DE DATOS
+    start = 1
+    stop  = 10000 
+    step  = 1
+    encontrado = False
+
+    #BÚSQUEDA DE FRACCIÓN IRREDUCIBLE
+    for dividendo in range(start, stop, step):
+        if(encontrado==True):
+            break
+        for divisor in range(start, stop, step):
+            if(encontrado==True):
+                break
+            resultado=dividendo/divisor
+            if(resultado==decimal and encontrado==False):
+                resultado=1
+                valor=(str(dividendo)+"/"+str(divisor))
+                encontrado=True
+    if encontrado == False:
+        resultado=2
+        
+    #RETURN
+    if resultado==1:
+        return valor
+    if resultado==2:
+        return "Vaya parece que no lo encontramos...prueba otro."
+
+                
+
+#PROGRAMA
 min=0.0001
 max=0.9999
 
-#Tomar dato
 while True:
-    decimal=float(input("Introduce un número entre 0.0001 y 0.9999 "))
-    if(decimal>=min and decimal<=max):
-        break
-print(decimal)
+    while True:
+        try:
+            decimal=float(input("Introduce un número entre 0.0001 y 0.9999 "))
+            if(decimal>=min and decimal<=max):
+                break
+            else:
+                print("Ese número no vale. Prueba de nuevo...")
+        except ValueError:
+            print("No has escrito un número. Prueba de nuevo...")
+    a=fraccion(decimal)
+    print(a)
 
-start = 1
-stop  = 10000 
-step  = 1
-encontrado = False
 
-for dividendo in range(start, stop, step):
-    if(encontrado==True):
-        break
-    for divisor in range(start, stop, step):
-        if(encontrado==True):
-            break
-        #print(dividendo , "/" , divisor)
-        resultado=dividendo/divisor
-        if(resultado==decimal and encontrado==False):
-            print(dividendo , "/" , divisor)
-            encontrado=True
-if encontrado == False:
-    print("¡Vaya parece que no lo encontramos...")            
-            
