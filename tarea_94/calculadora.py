c52708c49ee246658138e70392a8faf1c9edd1e9
retegi <retegi84@gmail.com>
2021-12-13 19:46:47 +0100

    Tarea 94

---
+++ tarea_94/calculadora.py
@@ -0,0 +1,14 @@
+#Tarea 94, el archivo calculadora.py es un módulo que contiene 4 funciones de cálculo.
+#Estas funciones pueden ser utilizadas de la siguiente manera: calculadora.suma(1,2)
+
+def suma(n1,n2):
+    return(n1+n2)
+
+def resta(n1,n2):
+    return(n1-n2)
+
+def producto(n1,n2):
+    return(n1*n2)
+
+def division(n1,n2):
+    return(n1/n2)
---
+++ tarea_94/calculos.py
@@ -0,0 +1,9 @@
+#Importamos módulo que contiene 4 funciones
+import calculadora
+
+#Hacemos uso del módulo y sus funciones.
+print(calculadora.suma(2,4))
+print(calculadora.resta(2,4))
+print(calculadora.producto(2,4))
+print(calculadora.division(2,4))
+
